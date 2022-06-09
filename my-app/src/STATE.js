35e798a1a440a385ede04671e5ed469cc1a9d7af
mmraihan <mm_raihan@live.com>
2022-06-09 18:07:53 +0600

    setState

--- my-app/src/STATE.js
+++ my-app/src/STATE.js
@@ -11,12 +11,27 @@ export default class STATE extends Component {
        count: 0,
     }
   }
-
+  handleIncrement=()=>{
+    console.log("Clicked");
+    this.setState({
+      count: this.state.count +1
+    })
+  }
   render() {
+   
+
+    const {count} = this.state //Destructure
     return (
     <div>
-     <h1> count: {this.state.count}</h1>
+     <h1> count: {count}</h1>
+        <button onClick={this.handleIncrement}>+</button>
+
     </div>
     )
   }
 }
+
+
+
+
+
