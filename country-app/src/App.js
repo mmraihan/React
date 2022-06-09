a493abced3636528c6e2399557cc2733a29e0b97
mmraihan <mm_raihan@live.com>
2022-06-09 20:13:28 +0600

    Fetch Data From API.

--- country-app/src/App.js
+++ country-app/src/App.js
@@ -1,7 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from "react";
+
+
+const url =`https://restcountries.com/v3.1/all`;
 
 function App() {
+
+  const [isLoading, setIsLoading]=useState(true);
+  const [error, setError]=useState(null);
+  const [countries, setCountries]=useState([]);
+
+ const fetchData = async (url)=>{
+   setIsLoading(true);
+
+
+   try {
+    const reponse = await fetch(url);
+    const data = await reponse.json();
+    setCountries(data);
+    setIsLoading(false);
+    setError(null);
+
+    console.log(countries)
+     
+   } catch (error) {
+     setIsLoading(false);
+     setError(error);
+   }
+
+ }
+
+  useEffect(() => {
+  fetchData(url)
+  }, [])
+  
+
+
   return (
     <div>
     App
