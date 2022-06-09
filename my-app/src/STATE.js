f7c9426f68e6554e0952b0d347dc281ce42afe32
mmraihan <mm_raihan@live.com>
2022-06-09 18:00:21 +0600

    State

--- my-app/src/STATE.js
+++ my-app/src/STATE.js
@@ -4,10 +4,18 @@ import React, { Component } from 'react'
 //Class component
 export default class STATE extends Component {
 
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+       count: 0,
+    }
+  }
+
   render() {
     return (
     <div>
-      {this.props.count}
+     <h1> count: {this.state.count}</h1>
     </div>
     )
   }
