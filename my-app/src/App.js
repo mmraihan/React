354fae6cdfbcd3638088a8ed8cafcec3a4933800
mmraihan <mm_raihan@live.com>
2022-07-03 12:30:55 +0600

    Child to parent data

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,9 +5,12 @@ import { Child } from './components/STATE_LIFTING/Child';
 
 function App() {
   const data ="I an from parent";
+  const handleChildData=(childData)=>{
+    console.log(childData)
+  }
   return (
     <div>
-      <Child data={data}/>
+      <Child data={data} onChildData={handleChildData}/>
     </div>
   
   )
--- my-app/src/components/STATE_LIFTING/Child.js
+++ my-app/src/components/STATE_LIFTING/Child.js
@@ -1,6 +1,10 @@
 import React from 'react'
 
 export const Child = (props) => {
+
+const data = "I am from Child"
+props.onChildData(data)
+
   return (
     <div>
         <p>I am  Child</p>
